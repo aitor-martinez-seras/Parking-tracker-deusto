1a8d541bf4b2d7152e57ecb790f55c2d73ae63df
Martinez Seras <aitor.martinez@tecnalia.com>
2022-04-10 21:04:22 +0200

    Last backup before changing the variable name handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 import sched, time
@@ -19,7 +20,6 @@ def get_general_DBS_parking_free_slots(dataframe, var_name):
     # Scrape the page where info is available
     page = requests.get('http://parking.deusto.es/')
     now = datetime.now(pytz.timezone('Europe/Madrid'))
-    # TODO: Logger
     logging.info(now.time())
     #Get HTML content
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -48,15 +48,16 @@ def get_general_DBS_parking_free_slots(dataframe, var_name):
     s.enter(delay=DELAY, priority=1, action=get_general_DBS_parking_free_slots, argument=(globals()[var_name],var_name))
 
 
-def save_csv(parking_data, var_name, dir_name, backup=False):
+def save_csv(parking_data, var_name, data_dir_name, backup=False):
     # To use this function for saving the backup also
     if backup is True:
-        var_path = os.path.join(dir_name, var_name + '.csv')
-        parking_data.to_csv(var_path, sep=';', decimal=',')
+        file_path = working_dir_path / data_dir_name / (var_name + ".csv")
+        file_path = os.path.join(data_dir_name, var_name + '.csv')
+        parking_data.to_csv(file_path, sep=';', decimal=',')
     else:
-        list_existing_files = os.listdir(dir_name)
+        list_existing_files = os.listdir(working_dir_path / data_dir_name)
         coincidences = []
-        var_path = ''
+        file_path = ''
         string_found = -1
         # Check in the directory if there is a file of the same day already
         for item in list_existing_files:
@@ -65,72 +66,85 @@ def save_csv(parking_data, var_name, dir_name, backup=False):
                 pos = len(var_name) + 1
                 coincidences.append(int(item[pos:pos+2]))
             else:
-                var_path = f'{var_name}_00.csv'
+                file_path = f'{var_name}_00.csv'
         # If there are coincidences, we sum 1 to not overwrite the .csv of that day
         if len(coincidences) > 0:
             # Extract the last item -> var_name + 'XX' and sum 1 to prevent the overwriting
             number_of_csv_today = str(max(coincidences)+1)
             if len(number_of_csv_today) == 2: # The case of two digit numbers
-                var_path = var_name + '_' + number_of_csv_today + '.csv'
+                file_path = var_name + '_' + number_of_csv_today + '.csv'
             elif len(number_of_csv_today) == 1: # The case of one digit numbers (we add a 0 before the number)
-                var_path = var_name + '_0' + number_of_csv_today + '.csv'
+                file_path = var_name + '_0' + number_of_csv_today + '.csv'
             else:
-                # TODO: Logger
                 logging.info(f'Number of .csv today is {number_of_csv_today}')
         elif len(coincidences) == 0:
             # If coincidences = [], that means either that there are not files in the directory or that the
             # files in the directory are of other days
-            var_path = f'{var_name}_00.csv'
+            file_path = f'{var_name}_00.csv'
         # Save to a .csv the data generated
-        var_path = os.path.join(dir_name,var_path)
+        file_path = working_dir_path / data_dir_name / file_path
         # TODO: Disjoint the array name of the .csv name
-        parking_data.to_csv(var_path, sep=';', decimal=',')
-        logging.info(f'{var_path} saved!')
+        parking_data.to_csv(file_path, sep=';', decimal=',')
+        logging.info(f'{file_path} saved!')
 
 def main():
-    # Make scheduler global
-    global s
-    # Define the delay (in seconds)
-    global DELAY
-    DELAY = 15
-    # TODO: Create a logger
     # Define logger basic configuration
     logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    # Check if Data is created
-    try:
-        os.mkdir('./Data')
-    except FileExistsError:
-        logging.info('Data directory already exist')
+
+    # Make scheduler, the DELAY and the working_dir_path constants global
+    global s, DELAY, working_dir_path
+    # Define the delay (in seconds)
+    DELAY = 15
+    # Define the current directory for the paths
+    working_dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
     # Constant definition
     log = []
     today = datetime.now(pytz.timezone('Europe/Madrid'))
     DIR_NAME = 'Data'
-    var_name = 'dataframe' + '_' + str(today.year) + '_' + str(today.month) + '_' + str(today.day)
+    # Check if Data directory is created
+    try:
+        os.mkdir(dir_path / "Data")
+    except FileExistsError:
+        logging.info('Data directory already exist')
+
     # TODO: The var_name is not needed to define the name of the .csv generated when saving the collected data, so
     #   in future optimization me must get rid of it, as it is not needed
+    var_name = 'dataframe' + '_' + str(today.year) + '_' + str(today.month) + '_' + str(today.day)
     globals()[var_name] = DataFrame(columns=['Date', 'Time', 'General', 'DBS'])
-    # Here we definde the time function and the sleep function
+
+    # Here we definde the time function and the sleep function for the scheduler
     s = sched.scheduler(time.time, time.sleep)
     # Enter the parameters to the s object
     s.enter(delay=DELAY, priority=1, action=get_general_DBS_parking_free_slots, argument=(globals()[var_name],var_name))
+
+    # We handle every exception to check if we are killing the program or some other exception happened, so in that case
+    # we can call the program to restart itself
     try:
-        s.run()
+        s.run() # Makes the scheduler start
     except BaseException as e:
-        # Hay que hacer que los csv vayan a unca carpeta
-        # Queda por añadir un if por si me falla a mitad de dia... hacer pruebas con archivo dimmie
-        # Hay que añadir a gitignore que no suba los .csv
+        # In case we have an exception of any kind, we want to save out current data to a .csv file
         save_csv(globals()[var_name], var_name, DIR_NAME)
         if issubclass(e.__class__, Exception):
-            logging.warning('Exception')
+            logging.warning('Exception:')
             main()
         # If we don't return to main, we exit the program
         logging.exception("Program will exit with following exception:")
 
-
+    # Just in case other exception happens, we ensure the data is saved in a backup file that we will rewrite every time
     finally:
         save_csv(globals()[var_name], 'last_run_backup', DIR_NAME, backup=True)
         logging.info("last run's backup saved correctly")
 
 if __name__ == '__main__':
-    main()
+    #main()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+    print(os.path.join(dir_path, "Data"))
+    dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    a = "dataframe"
+    print(dir_path / "Data" / (a + ".csv"))
+
+
+
