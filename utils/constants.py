55cc171a5bdb404ebe6973a26be71fe364095869
Martinez Seras <aitor.martinez@tecnalia.com>
2022-10-10 23:07:35 +0200

    Update the bucket

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
 TOKEN = "xDv026WEeftbSYMvNmjQP-R7nFuJ4hAAY1hxgfMHYOulsJ8Ho6WtG-Tu4ykJD2Z2EFRUtrIBveKOxSkVHFCqCQ=="
 ORG = "ams-home"
-BUCKET = "ams-bucket"
+BUCKET = "parking-deusto"
 DELAY = 15  # The delay between data points, in seconds
