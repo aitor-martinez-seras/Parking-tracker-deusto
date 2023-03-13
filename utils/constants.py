414565ea883b1491abf7ac7d681c2c5fcb25a84d
110414 <aitor.martinez@tecnalia.com>
2023-03-13 16:51:03 +0100

    update constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-# Influx DB information
+# Influx DB 
 TOKEN = "xDv026WEeftbSYMvNmjQP-R7nFuJ4hAAY1hxgfMHYOulsJ8Ho6WtG-Tu4ykJD2Z2EFRUtrIBveKOxSkVHFCqCQ=="
 ORG = "ams-home"
 BUCKET = "parking-deusto"
