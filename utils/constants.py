d82564efa1098150a406f05e3e181688fee15ad7
110414 <aitor.martinez@tecnalia.com>
2023-03-13 16:49:24 +0100

    update constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,11 @@
+from pathlib import Path
+
+# Influx DB information
 TOKEN = "xDv026WEeftbSYMvNmjQP-R7nFuJ4hAAY1hxgfMHYOulsJ8Ho6WtG-Tu4ykJD2Z2EFRUtrIBveKOxSkVHFCqCQ=="
 ORG = "ams-home"
 BUCKET = "parking-deusto"
 DELAY = 15  # The delay between data points, in seconds
+
+DATA_DIR_PATH = Path('Data')
+UNMERGED_DATA_DIR_PATH = DATA_DIR_PATH / 'raw_not_merged'
+RAW_MERGED_DATA_DIR_PATH = DATA_DIR_PATH / 'raw_merged'
