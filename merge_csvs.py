1dc5c87d3e24ca1302c2a201a53e2be4e1f6d234
110414 <aitor.martinez@tecnalia.com>
2023-07-05 09:55:31 +0200

    Add checks for monotonic increase in merge all csv

--- merge_csvs.py
+++ merge_csvs.py
@@ -1,8 +1,28 @@
 import os
+from datetime import datetime, timedelta
 
 import pandas as pd
 
 from utils.constants import UNMERGED_DATA_DIR_PATH, RAW_MERGED_DATA_DIR_PATH
+
+
+def check_monotonic_increasing_dates(df, reference):
+    t_before = datetime.strptime('06:00:00', '%H:%M:%S')
+    for t in df['time']:
+        # Check if the diff is negative
+        try:
+            t = datetime.strptime(t, '%H:%M:%S.%f')
+        except ValueError:
+            t = datetime.strptime(t, '%H:%M:%S')
+
+        if t - t_before < reference:
+            return False
+        
+        # Update t_before value
+        t_before = t
+
+    return True
+
 
 # File names pattern: dataframe_YYYY_MM_DD_XX.csv where XX is a number from
 # 00 to 99 representing the order of the dataframes in that day 
@@ -10,12 +30,14 @@ from utils.constants import UNMERGED_DATA_DIR_PATH, RAW_MERGED_DATA_DIR_PATH
 new_columns = ['date', 'time', 'general_parking', 'dbs_parking']
 RAW_MERGED_DATA_DIR_PATH.mkdir(exist_ok=True)
 
-data_files = os.listdir(UNMERGED_DATA_DIR_PATH)
+data_files = list(sorted(UNMERGED_DATA_DIR_PATH.iterdir()))
 same_day_files = 1
 
 separator = '_'
 
-for i, fname in enumerate(sorted(UNMERGED_DATA_DIR_PATH.iterdir())):
+reference_timedelta = timedelta(seconds=0)
+
+for i, fname in enumerate(data_files):
     
     same_day_files -= 1
 
@@ -34,26 +56,37 @@ for i, fname in enumerate(sorted(UNMERGED_DATA_DIR_PATH.iterdir())):
 
         # Search for files retrieved in the same day
         for next_fname in data_files[i+1:]:
-            next_date = next_fname.split('_')[1:4]
+            next_date = next_fname.name.split('_')[1:4]
             if next_date == date:
                 same_day_files += 1
             else:
                 break
 
-        same_day_file_names = data_files[i:i+same_day_files]
+        same_day_file_paths = data_files[i:i+same_day_files]
 
         df = pd.DataFrame(columns=new_columns)
-        for csv_file in same_day_file_names:
-            df_new = pd.read_csv(UNMERGED_DATA_DIR_PATH / csv_file, sep=';', index_col=0)
+        for csv_file in same_day_file_paths:
+            df_new = pd.read_csv(csv_file, sep=';', index_col=0)
             df_new.columns = new_columns
             df = pd.concat([df, df_new], ignore_index=True)
+
+        monotonic_increase = check_monotonic_increasing_dates(df, reference=reference_timedelta)
+
+        if monotonic_increase is False:
+            print(f'Found not a monotonic increase in {fname}')
+            print(f'The order of the files used is {same_day_file_paths}')
+            print('Raising error...')
+            raise ValueError('Error when merging csvs, exiting program!')
+        else:
+            print(f'{[x.name for x in same_day_file_paths]} correct!')
         
         if fname.name.split('_')[0] == 'dataframe':
             new_name = fname.name.split('_')[:4]
             new_name = separator.join(new_name) + '.csv'
             df.to_csv(RAW_MERGED_DATA_DIR_PATH / new_name, sep=';')
-
             print(f'Succesfully saved {new_name}')
+            print()
+
         else:
             print(f'File with name {fname.name} not saved, as does not comply to the pattern')
 
