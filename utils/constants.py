cb56b9953576e3994d1aa46b952d01a9a8b61efb
110414 <aitor.martinez@tecnalia.com>
2024-03-03 11:52:47 +0100

    Update constants

--- utils/constants.py
+++ utils/constants.py
@@ -6,6 +6,9 @@ from pathlib import Path
 
 NEW_COLUMNS = ['date', 'time', 'general_parking', 'dbs_parking']
 
+#### Common use constants ####
+
+WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 #### PATHS ####
 
@@ -20,6 +23,10 @@ DOTENV_PATH = CONFIG_PATH / '.env'
 DATA_DIR_PATH = WORKING_DIR_PATH / 'Data'
 UNMERGED_DATA_DIR_PATH = DATA_DIR_PATH / 'raw_not_merged'
 RAW_MERGED_DATA_DIR_PATH = DATA_DIR_PATH / 'raw_merged'
+CURATED_DATA_DIR_PATH = DATA_DIR_PATH / 'curated'
+CURATED_ALL_DATA = CURATED_DATA_DIR_PATH / 'all_data.csv'
+CURATED_GENERAL_PARKING_DATA = CURATED_DATA_DIR_PATH / 'general_parking.csv'
+CURATED_DBS_PARKING_DATA = CURATED_DATA_DIR_PATH / 'dbs_parking.csv'
 
 # Docker
 DOCKER_DOTENV_PATH = WORKING_DIR_PATH / 'docker/.env'
